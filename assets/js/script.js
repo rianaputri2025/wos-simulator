cd996d939973d0e8ab3f3e810f46cf1bc360b31b
rianaputri2025 <orianaputrigame@example.com>
2025-05-28 17:27:07 +0700

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  const overlay = document.getElementById('overlay');
+  sidebar.classList.toggle('active');
+  overlay.classList.toggle('active');
+}
